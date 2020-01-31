099323b9380804a491bfd6d49fba346d40ad168e
oleg <oleg.sharnikov@outlook.com>
2020-01-31 16:55:42 +0300

    Rename methods

--- optimal_distribution.py
+++ optimal_distribution.py
@@ -23,7 +23,7 @@ persons_to_count = persons_amount + rebuys
 max_chips_per_person = [ChiptSet(int(chip_set.amount / persons_to_count), chip_set.nominal) for chip_set in chips]
 
 
-def calc_chips(chip_set, sum_left):
+def calc_single_set(chip_set, sum_left):
     if sum_left - chip_set.amount * chip_set.nominal >= 0:
         return (chip_set,), sum_left - chip_set.amount * chip_set.nominal
     else:
@@ -31,7 +31,7 @@ def calc_chips(chip_set, sum_left):
         return (ChiptSet(chips_amount, chip_set.nominal),), sum_left - chips_amount * chip_set.nominal
 
 
-def take_more(chips, sum_left) -> tuple:
+def calculate(chips, sum_left) -> tuple:
     if sum_left == 0:
         return ()
     elif not chips:
@@ -40,10 +40,10 @@ def take_more(chips, sum_left) -> tuple:
     elif sum_left < 0:
         raise ArithmeticError("Bullshit calc. The sum is below 0")
     else:
-        calc_chip_set, new_sum_left = calc_chips(chips[0], sum_left)
-        return calc_chip_set + take_more(chips[1:], new_sum_left)
+        calc_chip_set, new_sum_left = calc_single_set(chips[0], sum_left)
+        return calc_chip_set + calculate(chips[1:], new_sum_left)
 
 
-result = take_more(max_chips_per_person, chips_per_person)
+result = calculate(max_chips_per_person, chips_per_person)
 
 [print(f"{chips_set.amount} of {chips_set.nominal}") for chips_set in result]
